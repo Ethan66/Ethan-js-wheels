4c4c487f3f395fdcf368e46148b067c7e5ceaf97
Ethan66 <425734130@qq.com>
2017-11-22 22:46:46 +0800

    将显示代码用js渲染

--- js/index.js
+++ js/index.js
@@ -1,99 +1,4 @@
-//显示、隐藏代码
-/*!function(){
-    var $arrow=$("#content").find(".arrow")
-    var width=$arrow.find("span").width();
-    $arrow.css("text-indent",2*width)
-}();
-$("#content").on("mouseenter",".demo .case",function(){
-    if($(this).find(".code").css("display")=="none"){
-        $(this).addClass("hover")
-        $(this).find(".arrow").animate({'text-indent':0},200)
-        $(this).find(".arrow span").animate({opacity:1},200)
-    }
-});
-$("#content").on("mouseleave",".demo .case",function(){
-    if($(this).find(".code").css("display")=="none"){
-        var width=$("#content").find(".arrow span").width()
-        $(this).removeClass("hover")
-        $(this).find(".arrow").animate({'text-indent':2*width},200)
-        $(this).find(".arrow span").animate({opacity:0},200)
-    }
-});
-$('#content').on("click",".demo .arrow",function(){
-    $(this).prev().slideToggle(function(){
-        if($(this).css("display")=="none"){
-            var width=$("#content").find(".arrow span").width()
-            $(this).next(".arrow").animate({'text-indent':2*width},200)
-            $(this).next(".arrow").find("span").animate({opacity:0},200)
-        }
-        else{
-            $(this).next(".arrow").find("i").addClass("up").next("span").text("隐藏代码")
-        }
-    })
-})*/
-var ShowOrHideCode=(function(){
-    var showOrHideCode1=function($ct){
-        this.init($ct)
-        this.bind()
-    }
-    showOrHideCode1.prototype={
-        init:function($ct){
-            this.$ct=$ct
-            this.$code=$ct.find(".code")
-            var $arrow=this.$arrow=$ct.find('.arrow')
-            var $arrSpan=this.$arrSpan=$arrow.find('span')
-            var spanWidth=this.spanWidth=$arrSpan.width()
-            $arrow.css("text-indent",2*spanWidth)
-        },
-        bind:function(){
-            var self=this;
-            self.$ct.on("mouseenter",function(){
-                if(self.$code.css("display")=="none"){
-                    $(this).addClass("hover")
-                    self.$arrow.animate({'text-indent':0},200)
-                    self.$arrSpan.animate({opacity:1},200)
-                }
-            })
-            self.$ct.on("mouseleave",function(){
-                if(self.$code.css("display")=="none"){
-                    $(this).removeClass("hover")
-                    self.$arrow.animate({'text-indent':2*self.spanWidth},200)
-                    self.$arrSpan.animate({opacity:0},200)
-                }
-            })
-            self.$arrow.on("click",function(){
-                self.$code.slideToggle(function(){
-                    if($(this).css("display")=="none"){
-                        self.$arrow.find("i").removeClass("up")
-                        self.$arrow.animate({'text-indent':2*self.spanWidth},200)
-                        self.$arrSpan.animate({opacity:0},200)
-                    }
-                    else{
-                        self.$arrow.find("i").addClass("up").next("span").text("隐藏代码")
-                    }
-                })
-            })
-            self.$code.on("click",".copy",function(){
-                self.copyCss(self.$code.find(".css")[0])
-            })
-        },
-        copyCss:function(ct){
-            ct.select(); // 选择对象
-            document.execCommand("Copy"); // 执行浏览器复制命令
-            alert("已复制好，可贴粘。");
-        }
-    }
-    return {
-        init:function($ct){
-            $ct.each(function(index,value){
-                new showOrHideCode1($(this))
-            })
-        }
-    }
-})()
-ShowOrHideCode.init($("#content .demo .case"))
-
-
+//生成导航
 var Nav=(function(){
     var data={
         Form:[['active','Radio','单选框'],['','Checkbox','多选框'],['','Input','输入框'],['','InputNumber','计数器'],['','Select','选择器'],['','Switch','开关']],
@@ -165,4 +70,125 @@ var Nav=(function(){
     }
 })()
 Nav.init($("#nav"))
+
+
+//生成复制的代码和SCSS样式
+var RenderCode=(function(){
+    var RenderCode1=function($ct){
+        this.init($ct)
+        // this.bind()
+    }
+    RenderCode1.prototype={
+        init:function($ct){
+            this.$ct=$ct
+            this.li=$ct.find("li").html()
+            console.log(this.li)
+        }
+    }
+    return {
+        init:function($ct){
+            $ct.each(function(idnex,value){
+                new RenderCode1($(this))
+            })
+        }
+    }
+})()
+RenderCode.init($(".demo .case"))
+
+
+
+//显示、隐藏代码
+/*!function(){
+ var $arrow=$("#content").find(".arrow")
+ var width=$arrow.find("span").width();
+ $arrow.css("text-indent",2*width)
+ }();
+ $("#content").on("mouseenter",".demo .case",function(){
+ if($(this).find(".code").css("display")=="none"){
+ $(this).addClass("hover")
+ $(this).find(".arrow").animate({'text-indent':0},200)
+ $(this).find(".arrow span").animate({opacity:1},200)
+ }
+ });
+ $("#content").on("mouseleave",".demo .case",function(){
+ if($(this).find(".code").css("display")=="none"){
+ var width=$("#content").find(".arrow span").width()
+ $(this).removeClass("hover")
+ $(this).find(".arrow").animate({'text-indent':2*width},200)
+ $(this).find(".arrow span").animate({opacity:0},200)
+ }
+ });
+ $('#content').on("click",".demo .arrow",function(){
+ $(this).prev().slideToggle(function(){
+ if($(this).css("display")=="none"){
+ var width=$("#content").find(".arrow span").width()
+ $(this).next(".arrow").animate({'text-indent':2*width},200)
+ $(this).next(".arrow").find("span").animate({opacity:0},200)
+ }
+ else{
+ $(this).next(".arrow").find("i").addClass("up").next("span").text("隐藏代码")
+ }
+ })
+ })*/
+var ShowOrHideCode=(function(){
+    var showOrHideCode1=function($ct){
+        this.init($ct)
+        this.bind()
+    }
+    showOrHideCode1.prototype={
+        init:function($ct){
+            this.$ct=$ct
+            this.$code=$ct.find(".code")
+            var $arrow=this.$arrow=$ct.find('.arrow')
+            var $arrSpan=this.$arrSpan=$arrow.find('span')
+            var spanWidth=this.spanWidth=$arrSpan.width()
+            $arrow.css("text-indent",2*spanWidth)
+        },
+        bind:function(){
+            var self=this;
+            self.$ct.on("mouseenter",function(){
+                if(self.$code.css("display")=="none"){
+                    $(this).addClass("hover")
+                    self.$arrow.animate({'text-indent':0},200)
+                    self.$arrSpan.animate({opacity:1},200)
+                }
+            })
+            self.$ct.on("mouseleave",function(){
+                if(self.$code.css("display")=="none"){
+                    $(this).removeClass("hover")
+                    self.$arrow.animate({'text-indent':2*self.spanWidth},200)
+                    self.$arrSpan.animate({opacity:0},200)
+                }
+            })
+            self.$arrow.on("click",function(){
+                self.$code.slideToggle(function(){
+                    if($(this).css("display")=="none"){
+                        self.$arrow.find("i").removeClass("up")
+                        self.$arrow.animate({'text-indent':2*self.spanWidth},200)
+                        self.$arrSpan.animate({opacity:0},200)
+                    }
+                    else{
+                        self.$arrow.find("i").addClass("up").next("span").text("隐藏代码")
+                    }
+                })
+            })
+            self.$code.on("click",".copy",function(){
+                self.copyCss(self.$code.find(".css")[0])
+            })
+        },
+        copyCss:function(ct){
+            ct.select(); // 选择对象
+            document.execCommand("Copy"); // 执行浏览器复制命令
+            alert("已复制好，可贴粘。");
+        }
+    }
+    return {
+        init:function($ct){
+            $ct.each(function(index,value){
+                new showOrHideCode1($(this))
+            })
+        }
+    }
+})()
+ShowOrHideCode.init($("#content .demo .case"))
 
