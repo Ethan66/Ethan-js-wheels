aa803d91ca0010b1e5fcd644e2db95edbcc44ede
Ethan66 <2064281269@qq.com>
2017-11-23 17:50:30 +0800

    添加组件新增功能（需要服务器）

--- js/index.js
+++ js/index.js
@@ -145,6 +145,71 @@ var RenderCode=(function(){
 })()
 RenderCode.init($(".model"))
 
+//生成添加按钮
+var AddModelBtn=(function(){
+    var AddModelBtn1=function($ct){
+        this.init($ct)
+        this.bind()
+    }
+    AddModelBtn1.prototype={
+        init:function($ct){
+            this.$ct=$ct
+        },
+        bind:function(){
+            var self=this;
+            self.render()
+            self.$ct.on("click","button",function(){
+                var id=self.$ct.attr("class")
+                $("."+id+"1").show();
+                // alert("此功能需要配合服务器，因为要保存添加的数据")
+            })
+        },
+        render:function(){
+           /* <button>
+                <svg class="icon" aria-hidden="true">
+                    <use xlink:href="#icon-iconjia1"></use>
+                </svg>
+                添加组件
+            </button>*/
+           var html='<button><svg class="icon" aria-hidden="true"><use xlink:href="#icon-iconjia1"></use></svg>添加组件</button>'
+           this.$ct.append(html)
+           /*<div class="pop addModelBtn1 none">
+                <div class="wrap">
+                    <h2>添加组件</h2>
+                    <div class="line">
+                        <p><span>组件名称</span><input type="text" placeholder="填写名称" /></p>
+                        <p><span>组件描述</span><input type="text" placeholder="组件描述" /></p>
+                    </div>
+                    <div class="line">
+                        <p><span>分类</span><input type="text" placeholder="组件分类" /></p>
+                        <p><span>分类描述</span><input type="text" placeholder="分类描述" /></p>
+                    </div>
+                    <div class="line">
+                        <p><span>代码</span><input type="text" /></p>
+                    </div>
+                    <div class="line">
+                        <button>立即添加</button><button class="cancel">取消</button>
+                    </div>
+                </div>
+            </div>*/
+           var html1='<div class="pop addModelBtn1 none"><div class="wrap"><h2>添加组件</h2>'
+            html1+='<div class="line"><p><span>组件名称</span><input type="text" placeholder="填写名称" /></p><p><span>组件描述</span><input type="text" placeholder="组件描述" /></p></div>'
+            html1+='<div class="line"><p><span>分类</span><input type="text" placeholder="组件分类" /></p><p><span>分类描述</span><input type="text" placeholder="分类描述" /></p></div>'
+            html1+='<div class="line"><p><span>代码</span><input type="text" /></p></div>'
+            html1+='<div class="line"> <button>立即添加</button><button class="cancel">取消</button></div>'
+            this.$ct.after(html1)
+        }
+    }
+    return {
+        init:function($ct){
+            $ct.each(function(index,value){
+                new AddModelBtn1($(this))
+            })
+        }
+    }
+})()
+AddModelBtn.init($(".addModelBtn"))
+
 
 
 //显示、隐藏代码
